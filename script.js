ba9fe05576ccd8684dd1a0172742b5109ad13964
cudenvn <cudenvn@gmail.com>
2019-01-03 18:03:20 +0700

    buttons

--- script.js
+++ script.js
@@ -1,14 +1,27 @@
-var buttons = document.querySelectorAll("span");
-var operators = ["+", "-", "x", "÷"];
-var decimalPlace = false;
-for (var i = 0; i < buttons.length; i++) {
-  buttons[i].onclick = function(e){
-    //store screen input to var
-    var outputScreen = document.querySelector("#screen");
-    var outputValues = outputScreen.innerHTML;
-    var btnValues = this.innerHTML;
+// let buttons = document.querySelectorAll('span')
+// let operators = ['+', '-', 'x', '÷']
+// let decimalPlace = false
+// for (var i = 0; i < buttons.length; i++) {
+//   buttons[i].onclick = function(e) {
+//     //store screen input to var
+//     console.log(buttons[i])
+//     var outputScreen = document.querySelector('#screen')
+//     var outputValues = outputScreen.innerHTML
+//     var btnValues = this.innerHTML
+//   }
+// }
 
+document.addEventListener('DOMContentLoaded', function() {
+  console.log('Hello World!')
+  let buttons = document.querySelectorAll('span')
+  for (var i = 0; i < buttons.length; i++) {
+    buttons[i].onclick = function(e) {
+      //store screen input to var
+      let outputScreen = document.querySelector('#screen')
+      let outputValues = outputScreen.innerHTML
+      let btnValue = this.innerHTML
+      outputScreen.innerHTML += btnValue
+      console.log(btnValue)
+    }
   }
-}
-
-//noooooooooooooooooooooooooooooooooo
+})
